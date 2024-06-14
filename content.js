f168542f75b7c9e3b14f3b9a8fe6c3cce050a28f
Moon145-iut <112395572+Moon145-iut@users.noreply.github.com>
2024-06-14 13:05:41 +0600

    content.js
    added imgur images

--- content.js
+++ content.js
@@ -28,7 +28,7 @@ const array = [
 ];
 
 
-const random = Math.floor(Math.random() * array.length);
+/*const random = Math.floor(Math.random() * array.length);
 
 const url = "https://cataas.com/cat";
 
@@ -36,5 +36,31 @@ const image = document.getElementById("cat");
 image.src = url;
 
 const text = document.getElementById("text");
-text.innerHTML = decodeURI(array[random]);
+text.innerHTML = decodeURI(array[random]);*/
+
+const images = [
+    'https://imgur.com/CfMbgeC',
+    'https://imgur.com/hc3M8ol',
+    'https://imgur.com/WuhMSMF',
+    'https://imgur.com/y36Ibpq0',
+    'https://imgur.com/AMn4aF5',
+    'https://imgur.com/jNgShO0',
+    'https://imgur.com/r1AamYG',
+    'https://imgur.com/OfxGOO4',
+    'https://imgur.com/3sAOBuS',
+    'https://imgur.com/wmOAmIM',
+    'https://imgur.com/dmTYqHL',
+    'https://imgur.com/VdBygKp',
+];
+
+
+function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+}
+
+window.onload = function() {
+    const imgElement = document.getElementById('cat');
+    imgElement.src = getRandomImage();
+}
 
